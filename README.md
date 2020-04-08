36bdea24ae77c2604c1121cd6e0c7cafda5694db
Matt Chapman <matt@mattchapman.io>
2020-04-09 11:29:37 +1200

    Init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## chattr.
+
+A simple chat app designed as a proof-of-concept and sandbox for using React with Socket.io
---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component{}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
