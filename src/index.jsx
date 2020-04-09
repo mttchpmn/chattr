d7835e82acd5e71c244bb239960f3e6417a279a1
Matt Chapman <matt@mattchapman.io>
2020-04-10 11:00:21 +1200

    Init commit of app min func

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,102 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import io from "socket.io-client";
 
-class App extends React.Component{}
+const socket = io.connect("http://localhost:4000");
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      msg: "",
+      messages: [],
+      name: "",
+    };
+  }
+
+  componentDidMount() {
+    socket.on("chat message", msg => {
+      const { messages } = this.state;
+      console.log(`${msg.name} : ${msg.text}`);
+      this.setState({ messages: [...messages, msg] });
+      console.log("this.state :", this.state);
+    });
+  }
+
+  handleTextChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  handleSubmit(e) {
+    const { msg, name } = this.state;
+
+    const payload = {
+      name,
+      text: msg,
+    };
+
+    e.preventDefault();
+    socket.emit("chat message", payload);
+    this.setState({ msg: "" });
+  }
+
+  renderChat() {
+    const { messages } = this.state;
+
+    return (
+      <div>
+        {messages.map((msg, i) => (
+          <div key={i}>
+            {msg.name} : {msg.text}
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  render() {
+    const { msg, name } = this.state;
+
+    return (
+      <div>
+        <h1>chattr.</h1>
+        <div>
+          Enter your name:
+          <input
+            name="name"
+            value={name}
+            onChange={e => this.handleTextChange(e)}
+          />
+        </div>
+        {this.renderChat()}
+        <form
+          style={{ position: "fixed", bottom: 0, width: "100%", padding: 5 }}
+        >
+          <input
+            name="msg"
+            value={msg}
+            onChange={e => this.handleTextChange(e)}
+            placeholder="Type a message"
+            autoComplete="off"
+            style={{ width: "80%" }}
+          />
+          <button
+            type="submit"
+            onClick={e => this.handleSubmit(e)}
+            style={{ width: "10%" }}
+          >
+            Send
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
