c3481f7235639899c4558b5c766bbc09bd670c38
Matt Chapman <matt@mattchapman.io>
2020-04-10 18:09:21 +1200

    Remove console.logs

--- server.js
+++ server.js
@@ -3,28 +3,20 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const PORT = process.env.PORT || 4000;
 
-io.on("create", room => {
-  console.log("CREATE called");
-  console.log("room", room);
-});
-
 io.on("connection", socket => {
   const { id } = socket;
   console.log(`${id} has connected`);
 
   socket.on("create", () => {
-    console.log(`Create called by ${id}`);
-
     io.to(id).emit("create", id);
   });
 
   socket.on("join", joinID => {
-    console.log(`JOIN : ${joinID}`);
     socket.join(joinID);
+    io.to(id).emit("join", joinID);
   });
 
   socket.on("chat message", ({ name, text }) => {
-    console.log(`${name} : ${text}`);
     io.emit("chat message", { name, text });
   });
 });
