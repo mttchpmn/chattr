f02e3cde5b48cc5e9b9d243936faee39e5ab518c
Matt Chapman <matt@mattchapman.io>
2020-04-11 00:56:40 +1200

    Serve react app from server

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
+const path = require("path");
 const PORT = process.env.PORT || 4000;
 
+// Serve React app from server
+app.use(express.static(path.join(__dirname, "/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/build/index.html"));
+});
+
+// Handle Socket communication
 io.on("connection", socket => {
   const { id } = socket;
   console.log(`${id} has connected`);
