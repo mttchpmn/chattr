81fd743dd0de6480b42cac7fc9b0d288dd8d7cf6
Matt Chapman <matt@mattchapman.io>
2020-04-12 00:12:43 +1200

    Update header design"

--- src/components/ChatHeader/index.jsx
+++ src/components/ChatHeader/index.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
+const colors = [
+  "#3CBCFC",
+  "#9878F8",
+  "#F85898",
+  "#FCA044",
+  "#B8F818",
+  "#58F898",
+];
+
 function ChatHeader({ name, roomID }) {
-  const [color, setColor] = React.useState("#aaa");
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  const [color, setColor] = React.useState("#505050");
+
   return (
-    <div className="nes-container is-rounded" style={{ margin: 10 }}>
+    <div
+      className="nes-container is-rounded"
+      style={{
+        margin: 10,
+        backgroundColor: randomColor,
+      }}
+    >
       <h1>chattr.</h1>
       <div style={{ display: "flex", justifyContent: "space-between" }}>
         <span>Hi {name}!</span>
@@ -12,12 +29,8 @@ function ChatHeader({ name, roomID }) {
           text={roomID}
           onCopy={() => alert("Copied Room ID to clipboard!")}
         >
-          <span
-            onMouseEnter={() => setColor("#209cee")}
-            onMouseLeave={() => setColor("#aaa")}
-            style={{ textAlign: "right", color: color }}
-          >
-            Room ID: {roomID}
+          <span style={{ textAlign: "right", color: color }}>
+            Room ID: {roomID}&#x1f4cb;
           </span>
         </CopyToClipboard>
       </div>
